3d58a90ac30fb1fd17a0bbaa42438d87bbb5cac5
manot40 <kevinsandiho290100@gmail.com>
2022-05-08 21:35:57 +0700

    Typings

--- type.d.ts
+++ type.d.ts
@@ -1,26 +1,40 @@
 declare type TQuote = {
   content: string;
   author: string;
-}
+};
 
 declare type TSkill = {
   name: string;
   value: string;
-}
+};
 
 declare type TExperience = {
   title: string;
   description: string;
   date: string;
   type: "job" | "education";
-}
+};
+
+declare type TSocial = {
+  github: string;
+  twitter: string;
+  linkedin: string;
+  whatsapp: string;
+};
+
+declare type TProject = {};
 
 declare type APIData = {
-  about?: string;
-  quote?: TQuote;
-  skills?: any[];
-  experiences?: any[];
-  projects: any;
+  avatarUrl: string;
+  fullname: string;
+  nickname: string;
+  greeting: string;
+  about: string;
+  quote: TQuote;
+  skills: any[];
+  experiences: any[];
+  projects: TProject | TProject[];
+  social: TSocial;
 };
 
 declare type APIRes<T> = {
